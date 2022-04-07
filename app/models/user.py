6ea98cf88df7c20c53ce08ae8e16cafabff41a69
Jose Arcidiacono <j2arcidiacono@outlook.com>
2022-04-07 09:08:56 -0300

    bug registro

--- app/models/user.py
+++ app/models/user.py
@@ -48,9 +48,9 @@ class User(object):
 	def register(cls, username, password, province, city, institute, email, name, surname, birthday):
 		con = get_db()
 
-		query="INSERT INTO public.user(username, password, province_id, city_id, email, name, surname, birthday) VALUES(%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"
+		query="INSERT INTO public.user(username, password, province_id, city_id, establishment_id, email, name, surname, birthday) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"
 		cursor = con.cursor(cursor_factory = psycopg2.extras.DictCursor)
-		cursor.execute(query, (username, password, province, city, email, name, surname, birthday))
+		cursor.execute(query, (username, password, province, city, institute, email, name, surname, birthday))
 		a = cursor.fetchone()[0]
 		query="INSERT INTO public.user_role VALUES(%s, '3')"
 		cursor.execute(query, (a,))
